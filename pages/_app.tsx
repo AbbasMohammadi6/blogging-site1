4376cd551265820537ff88aade696f5a455e7f0e
Abbas Mohammadi <abbasmohammadi6@gmail.com>
2021-04-18 13:13:23 +0430

    fix a typo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import React, { ComponentType } from "react";
 import "../styles/globals.css";
 import { Provider } from "react-redux";
-import store from "sotre/store";
+import store from "store/store";
 import { AppInitialProps } from "next/app";
 
 function MyApp({
